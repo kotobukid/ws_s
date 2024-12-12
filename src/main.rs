116b1d3fce9de261cb5798cd52eb19c414bbcbfc
kotobukid <kotobuki@daqua.jp>
2024-12-12 13:12:22 +0900

    server

---
+++ src/main.rs
@@ -0,0 +1,39 @@
+use futures_util::{future, StreamExt, TryStreamExt};
+use log::info;
+use std::net::SocketAddrV4;
+use tokio::net::{TcpListener, TcpStream};
+
+#[tokio::main]
+async fn main() -> anyhow::Result<()> {
+    let addr: SocketAddrV4 = "127.0.0.1:8080".parse()?;
+
+    let socket = TcpListener::bind(&addr).await;
+    let listener = socket.expect("Failed to bind socket");
+
+    println!("Listening on: {}", addr);
+    while let Ok((stream, _)) = listener.accept().await {
+        tokio::spawn(accept_connection(stream));
+    }
+
+    Ok(())
+}
+
+async fn accept_connection(stream: TcpStream) {
+    let addr = stream
+        .peer_addr()
+        .expect("connected streams should have a peer address");
+    info!("Peer address: {}", addr);
+
+    let ws_stream = tokio_tungstenite::accept_async(stream)
+        .await
+        .expect("Error during the websocket handshake occurred");
+
+    info!("New WebSocket connection: {}", addr);
+
+    let (write, read) = ws_stream.split();
+    // We should not forward messages other than text or binary.
+    read.try_filter(|msg| future::ready(msg.is_text() || msg.is_binary()))
+        .forward(write)
+        .await
+        .expect("Failed to forward messages")
+}
