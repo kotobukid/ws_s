fe47117a8419926a9312aa07e227669dceeb4f64
kotobukid <kotobuki@daqua.jp>
2024-12-25 13:40:34 +0900

    Nuxtはコンポーネント名はインポート不要で使用可能であった

--- front/app.vue
+++ front/app.vue
@@ -1,12 +1,11 @@
 <template>
   <div>
-    <Chat_front/>
+    <ChatFront/>
     <!--    <NuxtRouteAnnouncer />-->
     <!--    <NuxtWelcome />-->
   </div>
 </template>
 <script setup lang="ts">
-import Chat_front from "~/components/chat_front.vue";
 import {useWasmTest} from "~/composables/wasm_test";
 
 const {init, test, create_text_message, deserialize_text_message} = useWasmTest();
@@ -16,13 +15,11 @@ await init();
 const chat_message_b = create_text_message("taro", 42, "good morning");
 const chat_message_a = deserialize_text_message(chat_message_b);
 
-
 console.log({
       chat_message_b,
       chat_message_a
     }
 );
-
 
 fetch('/api/health.json').then(res => res.json()).then(json => {
   console.log(json);
